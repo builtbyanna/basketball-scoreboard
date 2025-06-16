8e1ca375f56815590cd25ad49d06d4d604ebc8a5
Anna <schusteranna00@gmail.com>
2025-06-16 21:10:16 +0200

    finish main project

--- index.js
+++ index.js
@@ -5,4 +5,29 @@ function plus1() {
     document.getElementById("home-count").textContent = home;
 }
 
-console.log(plus1)
+function plus2() {
+    home += 2;
+    document.getElementById("home-count").textContent = home;
+} 
+
+function plus3() {
+    home += 3;
+    document.getElementById("home-count").textContent = home;
+}
+
+let guest = 0;
+
+function gplus1() {
+    guest += 1;
+    document.getElementById("guest-count").textContent = guest;
+}
+
+function gplus2() {
+    guest += 2;
+    document.getElementById("guest-count").textContent = guest;
+} 
+
+function gplus3() {
+    guest += 3;
+    document.getElementById("guest-count").textContent = guest;
+}
